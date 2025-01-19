36423cb6a22aa264c055220d6ca965514ba4631d
Rui Freitas <dinnerpartysaboteur@gmail.com>
2025-01-19 20:53:55 +0000

    add oauth provider buttons in relevant auth views

--- lib/accomplish_web/controllers/user_registration_html.ex
+++ lib/accomplish_web/controllers/user_registration_html.ex
@@ -2,6 +2,7 @@ defmodule AccomplishWeb.UserRegistrationHTML do
   use AccomplishWeb, :html
 
   import SaladUI.Form
+  import AccomplishWeb.Components.OAuthComponents
 
   embed_templates "user_registration_html/*"
 end
--- lib/accomplish_web/controllers/user_session_html.ex
+++ lib/accomplish_web/controllers/user_session_html.ex
@@ -2,6 +2,7 @@ defmodule AccomplishWeb.UserSessionHTML do
   use AccomplishWeb, :html
 
   import SaladUI.Form
+  import AccomplishWeb.Components.OAuthComponents
 
   embed_templates "user_session_html/*"
 end
