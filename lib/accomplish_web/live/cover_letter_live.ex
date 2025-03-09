cdd4698e7815135433e5c504cb9b769de7bd213a
Rui Freitas <rodloboz@hey.com>
2025-03-09 22:42:10 +0000

    Acc 134 cover letter helpers (#87)
    * add covver letter helpers
    
    * add moduledoc

--- lib/accomplish_web/live/cover_letter_live.ex
+++ lib/accomplish_web/live/cover_letter_live.ex
@@ -5,7 +5,9 @@ defmodule AccomplishWeb.CoverLetterLive do
   alias Accomplish.CoverLetters
 
   import AccomplishWeb.StringHelpers
+  import AccomplishWeb.CoverLetterHelpers
   import AccomplishWeb.Layout
+  import AccomplishWeb.Shadowrun.Tooltip
 
   def render(assigns) do
     ~H"""
@@ -27,6 +29,45 @@ defmodule AccomplishWeb.CoverLetterLive do
           </:title>
         </.page_header>
       </:page_header>
+
+      <section class="max-w-3xl mx-auto px-6 lg:px-8 mt-24">
+        <div class="space-y-4 mb-6">
+          <.shadow_input
+            field={@form[:title]}
+            placeholder="Cover letter title"
+            class="text-[25px] tracking-tighter hover:cursor-text"
+            phx-blur="save_field"
+            phx-value-field={@form[:title].field}
+          />
+        </div>
+        <div class="flex justify-start gap-2 my-2">
+          <.tooltip>
+            <.shadow_select_input
+              id={"status_select_#{@form.id}"}
+              field={@form[:status]}
+              prompt="Change status"
+              value={@form[:status].value}
+              options={options_for_cover_letter_status()}
+              on_select="save_field"
+              variant="transparent"
+            />
+            <.tooltip_content side="bottom">
+              <p>Change status</p>
+            </.tooltip_content>
+          </.tooltip>
+        </div>
+        <div class="mt-12 space-y-2">
+          <.shadow_input
+            field={@form[:content]}
+            type="textarea"
+            placeholder="Highlight your skills, experience, and passion for this role..."
+            class="text-[14px] font-light hover:cursor-text"
+            socket={@socket}
+            phx-blur="save_field"
+            phx-value-field={@form[:content].field}
+          />
+        </div>
+      </section>
     </.layout>
     """
   end
@@ -47,6 +88,23 @@ defmodule AccomplishWeb.CoverLetterLive do
       {:ok, socket}
     else
       :error -> {:ok, push_navigate(socket, to: ~p"/job_applications")}
+    end
+  end
+
+  def handle_event("save_field", %{"field" => field, "value" => value}, socket) do
+    cover_letter = socket.assigns.cover_letter
+    changes = %{field => value}
+
+    case CoverLetters.update_cover_letter(cover_letter, changes) do
+      {:ok, updated_cover_letter} ->
+        form =
+          updated_cover_letter
+          |> CoverLetters.change_cover_letter()
+          |> to_form()
+
+        {:noreply,
+         socket
+         |> assign(form: form)}
     end
   end
 
