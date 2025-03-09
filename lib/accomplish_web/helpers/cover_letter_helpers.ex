05c7b147c978c9b2788b21281cebbaba66a66975
Rui Freitas <dinnerpartysaboteur@gmail.com>
2025-03-09 22:37:41 +0000

    add covver letter helpers

---
+++ lib/accomplish_web/helpers/cover_letter_helpers.ex
@@ -0,0 +1,27 @@
+defmodule AccomplishWeb.CoverLetterHelpers do
+  def options_for_cover_letter_status do
+    [
+      %{
+        label: "Draft",
+        value: :draft,
+        icon: "hero-pencil-square",
+        color: "text-slate-400",
+        shortcut: "1"
+      },
+      %{
+        label: "Final",
+        value: :final,
+        icon: "hero-document-check",
+        color: "text-green-600",
+        shortcut: "2"
+      },
+      %{
+        label: "Submitted",
+        value: :interviewing,
+        icon: "hero-envelope",
+        color: "text-purple-600",
+        shortcut: "3"
+      }
+    ]
+  end
+end
