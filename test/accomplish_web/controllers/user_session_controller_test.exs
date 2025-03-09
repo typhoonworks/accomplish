df69c13c8d650c2634ec38da0ebff98925b61602
Rui Freitas <rodloboz@hey.com>
2025-03-09 09:24:08 +0000

    ACC-129 - Minimalistic landing page (#79)
    * ACC-129 - Minimalistic landing page
    
    * fix tests
    
    * update footer

--- test/accomplish_web/controllers/user_session_controller_test.exs
+++ test/accomplish_web/controllers/user_session_controller_test.exs
@@ -35,8 +35,7 @@ defmodule AccomplishWeb.UserSessionControllerTest do
       # Now do a logged in request and assert on the menu
       conn = get(conn, ~p"/")
       response = html_response(conn, 200)
-      assert response =~ ~p"/settings/account/security"
-      assert response =~ ~p"/logout"
+      assert response =~ ~p"/mission_control"
     end
 
     test "logs the user in with remember me", %{conn: conn, user: user} do
