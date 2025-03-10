47faa3ccb2b4f16e4d879fa5aa51b223c29f38d1
Rui Freitas <dinnerpartysaboteur@gmail.com>
2025-03-10 08:34:58 +0000

    add handle info for cover letters events in application live

--- lib/accomplish_web/live/job_applications_live/application.ex
+++ lib/accomplish_web/live/job_applications_live/application.ex
@@ -770,6 +770,10 @@ defmodule AccomplishWeb.JobApplicationsLive.Application do
     handle_activity(event, socket)
   end
 
+  def handle_info({CoverLetters, _}, socket) do
+    {:noreply, socket}
+  end
+
   defp handle_activity(%{name: "activity.logged"} = event, socket) do
     activity = %{event.activity | entity: event.entity, context: event.context}
     {:noreply, stream_insert(socket, :activities, activity, at: 0)}
